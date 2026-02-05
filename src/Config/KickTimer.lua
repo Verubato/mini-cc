91063afdfe70086473af066533c5d83402002ec3
Verubato <>
2026-02-05 20:41:04 +1100

    Updated text.

--- src/Config/KickTimer.lua
+++ src/Config/KickTimer.lua
@@ -11,19 +11,19 @@ config.KickTimer = M
 
 function M:Build()
 	local db = mini:GetSavedVars()
-    local columns = 3
-    local columnWidth = mini:ColumnWidth(columns, 0, 0)
-    local horizontalSpacing = mini.HorizontalSpacing
+	local columns = 3
+	local columnWidth = mini:ColumnWidth(columns, 0, 0)
+	local horizontalSpacing = mini.HorizontalSpacing
 
 	local panel = CreateFrame("Frame")
 	local title = panel:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
 	title:SetPoint("TOPLEFT", 0, -verticalSpacing)
 	title:SetText("Kick timer")
 
-    local text = mini:TextLine({
-        Parent = panel,
-        Text = "Enable if you are:"
-    })
+	local text = mini:TextLine({
+		Parent = panel,
+		Text = "Enable if you are:",
+	})
 
 	text:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -verticalSpacing)
 
@@ -55,8 +55,8 @@ function M:Build()
 		end,
 	})
 
-    casterEnabled:SetPoint("LEFT", panel, "LEFT", columnWidth, 0)
-    casterEnabled:SetPoint("TOP", healerEnabled, "TOP", 0, o)
+	casterEnabled:SetPoint("LEFT", panel, "LEFT", columnWidth, 0)
+	casterEnabled:SetPoint("TOP", healerEnabled, "TOP", 0, o)
 
 	local allEnabled = mini:Checkbox({
 		Parent = panel,
@@ -71,8 +71,8 @@ function M:Build()
 		end,
 	})
 
-    allEnabled:SetPoint("LEFT", panel, "LEFT", columnWidth * 2, 0)
-    allEnabled:SetPoint("TOP", healerEnabled, "TOP", 0, 0)
+	allEnabled:SetPoint("LEFT", panel, "LEFT", columnWidth * 2, 0)
+	allEnabled:SetPoint("TOP", healerEnabled, "TOP", 0, 0)
 
 	local iconSizeSlider = mini:Slider({
 		Parent = panel,
@@ -84,7 +84,7 @@ function M:Build()
 			db.KickTimer.Icons.Size = mini:ClampInt(value, 20, 120, 50)
 			config:Apply()
 		end,
-        Width = columns * columnWidth - horizontalSpacing,
+		Width = columns * columnWidth - horizontalSpacing,
 		Min = 20,
 		Max = 120,
 		Step = 1,
@@ -107,6 +107,8 @@ function M:Build()
 			"But if you are facing RMP, then it has to use the rogue kick cooldown of 15 seconds.",
 			"It doesn't know whether the rogue or mage kicked you, so if no kicks are on cooldown then it has to assume the worst case scenario and choose the rogue's cooldown.",
 			"If both kicks get used, then it could know that one cooldown is 15 seconds and the other one is 24 seconds but I haven't coded in this logic yet.",
+			"",
+			"Honestly I don't even know if it's worth having this in the addon, let me know your thoughts.",
 		},
 	})
 
